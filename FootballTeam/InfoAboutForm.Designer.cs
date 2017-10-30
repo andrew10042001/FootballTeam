7f8ef9587faae2e4ecf2054f52e8af9abf77b219
Anna Stepaniuk <annstepaniuk12@gmail.com>
2017-10-30 17:58:08 +0200

    Add Forms to project

--- FootballTeam/InfoAboutForm.Designer.cs
+++ FootballTeam/InfoAboutForm.Designer.cs
@@ -28,11 +28,203 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InfoAboutForm));
+            this.dg_players = new System.Windows.Forms.DataGridView();
+            this.IdColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.RoleColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.AgeColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NameColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PositionColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.EfficiencyColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.gb_seach = new System.Windows.Forms.GroupBox();
+            this.cb_team = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cb_country = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cb_city = new System.Windows.Forms.ComboBox();
+            this.btn_search = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dg_players)).BeginInit();
+            this.gb_seach.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dg_players
+            // 
+            this.dg_players.AllowUserToAddRows = false;
+            this.dg_players.AllowUserToDeleteRows = false;
+            this.dg_players.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.dg_players.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dg_players.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.IdColumn,
+            this.RoleColumn,
+            this.AgeColumn,
+            this.NameColumn,
+            this.PositionColumn,
+            this.EfficiencyColumn});
+            this.dg_players.Location = new System.Drawing.Point(12, 71);
+            this.dg_players.Name = "dg_players";
+            this.dg_players.ReadOnly = true;
+            this.dg_players.Size = new System.Drawing.Size(808, 385);
+            this.dg_players.TabIndex = 1;
+            // 
+            // IdColumn
+            // 
+            this.IdColumn.HeaderText = "Id";
+            this.IdColumn.Name = "IdColumn";
+            this.IdColumn.ReadOnly = true;
+            this.IdColumn.Visible = false;
+            this.IdColumn.Width = 5;
+            // 
+            // RoleColumn
+            // 
+            this.RoleColumn.HeaderText = "Role";
+            this.RoleColumn.Name = "RoleColumn";
+            this.RoleColumn.ReadOnly = true;
+            // 
+            // AgeColumn
+            // 
+            this.AgeColumn.HeaderText = "Age";
+            this.AgeColumn.Name = "AgeColumn";
+            this.AgeColumn.ReadOnly = true;
+            // 
+            // NameColumn
+            // 
+            this.NameColumn.HeaderText = "Name";
+            this.NameColumn.Name = "NameColumn";
+            this.NameColumn.ReadOnly = true;
+            this.NameColumn.Width = 200;
+            // 
+            // PositionColumn
+            // 
+            this.PositionColumn.HeaderText = "Position";
+            this.PositionColumn.Name = "PositionColumn";
+            this.PositionColumn.ReadOnly = true;
+            this.PositionColumn.Width = 150;
+            // 
+            // EfficiencyColumn
+            // 
+            this.EfficiencyColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.EfficiencyColumn.HeaderText = "Efficiency";
+            this.EfficiencyColumn.Name = "EfficiencyColumn";
+            this.EfficiencyColumn.ReadOnly = true;
+            // 
+            // gb_seach
+            // 
+            this.gb_seach.BackColor = System.Drawing.Color.Transparent;
+            this.gb_seach.Controls.Add(this.cb_team);
+            this.gb_seach.Controls.Add(this.label3);
+            this.gb_seach.Controls.Add(this.cb_country);
+            this.gb_seach.Controls.Add(this.label2);
+            this.gb_seach.Controls.Add(this.cb_city);
+            this.gb_seach.Controls.Add(this.btn_search);
+            this.gb_seach.Controls.Add(this.label1);
+            this.gb_seach.ForeColor = System.Drawing.Color.White;
+            this.gb_seach.Location = new System.Drawing.Point(12, 12);
+            this.gb_seach.Name = "gb_seach";
+            this.gb_seach.Size = new System.Drawing.Size(808, 53);
+            this.gb_seach.TabIndex = 2;
+            this.gb_seach.TabStop = false;
+            this.gb_seach.Text = "Search";
+            // 
+            // cb_team
+            // 
+            this.cb_team.FormattingEnabled = true;
+            this.cb_team.Location = new System.Drawing.Point(484, 20);
+            this.cb_team.Name = "cb_team";
+            this.cb_team.Size = new System.Drawing.Size(201, 21);
+            this.cb_team.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(425, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Team:";
+            // 
+            // cb_country
+            // 
+            this.cb_country.FormattingEnabled = true;
+            this.cb_country.Location = new System.Drawing.Point(91, 20);
+            this.cb_country.Name = "cb_country";
+            this.cb_country.Size = new System.Drawing.Size(132, 21);
+            this.cb_country.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(17, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Country:";
+            // 
+            // cb_city
+            // 
+            this.cb_city.FormattingEnabled = true;
+            this.cb_city.Location = new System.Drawing.Point(274, 20);
+            this.cb_city.Name = "cb_city";
+            this.cb_city.Size = new System.Drawing.Size(145, 21);
+            this.cb_city.TabIndex = 2;
+            // 
+            // btn_search
+            // 
+            this.btn_search.BackColor = System.Drawing.Color.Navy;
+            this.btn_search.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_search.Location = new System.Drawing.Point(704, 13);
+            this.btn_search.Name = "btn_search";
+            this.btn_search.Size = new System.Drawing.Size(98, 31);
+            this.btn_search.TabIndex = 1;
+            this.btn_search.Text = "SEARCH";
+            this.btn_search.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(229, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "City:";
+            // 
+            // InfoAboutForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(833, 468);
+            this.Controls.Add(this.dg_players);
+            this.Controls.Add(this.gb_seach);
+            this.Name = "InfoAboutForm";
             this.Text = "InfoAbout";
+            ((System.ComponentModel.ISupportInitialize)(this.dg_players)).EndInit();
+            this.gb_seach.ResumeLayout(false);
+            this.gb_seach.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dg_players;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IdColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn RoleColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn AgeColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NameColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PositionColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn EfficiencyColumn;
+        private System.Windows.Forms.GroupBox gb_seach;
+        private System.Windows.Forms.ComboBox cb_team;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cb_country;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cb_city;
+        private System.Windows.Forms.Button btn_search;
+        private System.Windows.Forms.Label label1;
     }
 }
--- FootballTeam/MainForm.Designer.cs
+++ FootballTeam/MainForm.Designer.cs
@@ -29,26 +29,145 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btn_showteaminfo = new System.Windows.Forms.Button();
+            this.lb_name_secondteam = new System.Windows.Forms.Label();
+            this.lb_name_firstteam = new System.Windows.Forms.Label();
+            this.btn_moreinfo = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lb_goals_secondteam = new System.Windows.Forms.Label();
+            this.lb_goals_firstteam = new System.Windows.Forms.Label();
+            this.cb_secondteam = new System.Windows.Forms.ComboBox();
+            this.btn_play = new System.Windows.Forms.Button();
+            this.cb_firstteam = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
-            // comboBox1
+            // btn_showteaminfo
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(32, 54);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 0;
+            this.btn_showteaminfo.BackColor = System.Drawing.Color.Green;
+            this.btn_showteaminfo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_showteaminfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_showteaminfo.ForeColor = System.Drawing.Color.White;
+            this.btn_showteaminfo.Location = new System.Drawing.Point(-2, -1);
+            this.btn_showteaminfo.Name = "btn_showteaminfo";
+            this.btn_showteaminfo.Size = new System.Drawing.Size(637, 31);
+            this.btn_showteaminfo.TabIndex = 22;
+            this.btn_showteaminfo.Text = "БАЗА ДАНИХ КОМАНД";
+            this.btn_showteaminfo.UseVisualStyleBackColor = false;
             // 
-            // button1
+            // lb_name_secondteam
             // 
-            this.button1.Location = new System.Drawing.Point(272, 54);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.lb_name_secondteam.AutoSize = true;
+            this.lb_name_secondteam.BackColor = System.Drawing.Color.Transparent;
+            this.lb_name_secondteam.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lb_name_secondteam.ForeColor = System.Drawing.Color.White;
+            this.lb_name_secondteam.Location = new System.Drawing.Point(381, 291);
+            this.lb_name_secondteam.Name = "lb_name_secondteam";
+            this.lb_name_secondteam.Size = new System.Drawing.Size(52, 18);
+            this.lb_name_secondteam.TabIndex = 21;
+            this.lb_name_secondteam.Text = "label7";
+            // 
+            // lb_name_firstteam
+            // 
+            this.lb_name_firstteam.AutoSize = true;
+            this.lb_name_firstteam.BackColor = System.Drawing.Color.Transparent;
+            this.lb_name_firstteam.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lb_name_firstteam.ForeColor = System.Drawing.Color.White;
+            this.lb_name_firstteam.Location = new System.Drawing.Point(126, 291);
+            this.lb_name_firstteam.Name = "lb_name_firstteam";
+            this.lb_name_firstteam.Size = new System.Drawing.Size(52, 18);
+            this.lb_name_firstteam.TabIndex = 20;
+            this.lb_name_firstteam.Text = "label6";
+            // 
+            // btn_moreinfo
+            // 
+            this.btn_moreinfo.BackColor = System.Drawing.Color.Green;
+            this.btn_moreinfo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_moreinfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_moreinfo.ForeColor = System.Drawing.Color.White;
+            this.btn_moreinfo.Location = new System.Drawing.Point(93, 352);
+            this.btn_moreinfo.Name = "btn_moreinfo";
+            this.btn_moreinfo.Size = new System.Drawing.Size(450, 49);
+            this.btn_moreinfo.TabIndex = 19;
+            this.btn_moreinfo.Text = "Переглянути подробиці гри";
+            this.btn_moreinfo.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(379, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Оберіть команду";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(61, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Оберіть команду";
+            // 
+            // lb_goals_secondteam
+            // 
+            this.lb_goals_secondteam.AutoSize = true;
+            this.lb_goals_secondteam.BackColor = System.Drawing.Color.Transparent;
+            this.lb_goals_secondteam.Font = new System.Drawing.Font("Microsoft Sans Serif", 159.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lb_goals_secondteam.ForeColor = System.Drawing.Color.White;
+            this.lb_goals_secondteam.Location = new System.Drawing.Point(350, 78);
+            this.lb_goals_secondteam.Name = "lb_goals_secondteam";
+            this.lb_goals_secondteam.Size = new System.Drawing.Size(220, 241);
+            this.lb_goals_secondteam.TabIndex = 16;
+            this.lb_goals_secondteam.Text = "0";
+            // 
+            // lb_goals_firstteam
+            // 
+            this.lb_goals_firstteam.AutoSize = true;
+            this.lb_goals_firstteam.BackColor = System.Drawing.Color.Transparent;
+            this.lb_goals_firstteam.Font = new System.Drawing.Font("Microsoft Sans Serif", 159.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lb_goals_firstteam.ForeColor = System.Drawing.Color.White;
+            this.lb_goals_firstteam.Location = new System.Drawing.Point(85, 78);
+            this.lb_goals_firstteam.Name = "lb_goals_firstteam";
+            this.lb_goals_firstteam.Size = new System.Drawing.Size(220, 241);
+            this.lb_goals_firstteam.TabIndex = 15;
+            this.lb_goals_firstteam.Text = "0";
+            // 
+            // cb_secondteam
+            // 
+            this.cb_secondteam.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cb_secondteam.FormattingEnabled = true;
+            this.cb_secondteam.Location = new System.Drawing.Point(382, 51);
+            this.cb_secondteam.Name = "cb_secondteam";
+            this.cb_secondteam.Size = new System.Drawing.Size(192, 28);
+            this.cb_secondteam.TabIndex = 14;
+            // 
+            // btn_play
+            // 
+            this.btn_play.BackColor = System.Drawing.Color.Green;
+            this.btn_play.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_play.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_play.ForeColor = System.Drawing.Color.White;
+            this.btn_play.Location = new System.Drawing.Point(278, 49);
+            this.btn_play.Name = "btn_play";
+            this.btn_play.Size = new System.Drawing.Size(75, 30);
+            this.btn_play.TabIndex = 13;
+            this.btn_play.Text = "PLAY";
+            this.btn_play.UseVisualStyleBackColor = false;
+            // 
+            // cb_firstteam
+            // 
+            this.cb_firstteam.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cb_firstteam.FormattingEnabled = true;
+            this.cb_firstteam.Location = new System.Drawing.Point(64, 49);
+            this.cb_firstteam.Name = "cb_firstteam";
+            this.cb_firstteam.Size = new System.Drawing.Size(192, 28);
+            this.cb_firstteam.TabIndex = 12;
             // 
             // MainForm
             // 
@@ -56,18 +175,37 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(632, 419);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btn_showteaminfo);
+            this.Controls.Add(this.lb_name_secondteam);
+            this.Controls.Add(this.lb_name_firstteam);
+            this.Controls.Add(this.btn_moreinfo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lb_goals_secondteam);
+            this.Controls.Add(this.lb_goals_firstteam);
+            this.Controls.Add(this.cb_secondteam);
+            this.Controls.Add(this.btn_play);
+            this.Controls.Add(this.cb_firstteam);
             this.Name = "MainForm";
             this.Text = "Football";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_showteaminfo;
+        private System.Windows.Forms.Label lb_name_secondteam;
+        private System.Windows.Forms.Label lb_name_firstteam;
+        private System.Windows.Forms.Button btn_moreinfo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lb_goals_secondteam;
+        private System.Windows.Forms.Label lb_goals_firstteam;
+        private System.Windows.Forms.ComboBox cb_secondteam;
+        private System.Windows.Forms.Button btn_play;
+        private System.Windows.Forms.ComboBox cb_firstteam;
     }
 }
 
